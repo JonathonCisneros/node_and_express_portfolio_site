81c536f416d4dde1b8ea145f16fcd267be2dbf0c
Jonathon <cisneros.jonathon@gmail.com>
2019-04-22 20:45:19 -0700

    move images under public

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { data } = require('./data.json');
+//const { data } = require('./data.json');
 
 const app = express();
 
@@ -13,6 +13,17 @@ const aboutRoutes = require('./routes/about');
 app.use(mainRoutes);
 app.use('/about', aboutRoutes);
 
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error');
+});
 
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000!')
